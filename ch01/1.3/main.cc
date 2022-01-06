e9be67737d4473a825c93c2432853fc6a0380986
tobyguo <tobyguo@tencent.com>
2022-01-06 22:49:50 +0800

    feat: init

---
+++ ch01/1.3/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  // std::cout << "Enter two number:" << std::endl;
+  // int v1 = 0, v2 = 0;
+  // std::cin >> v1 >> v2;
+  // std::cout << "The sum of " << v1 << " + " << v2 << " is " << v1 + v2
+  //           << std::endl;
+
+  std::cout << "Hello, World." << std::endl;
+  return 0;
+}
